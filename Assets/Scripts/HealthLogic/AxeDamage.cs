3461c8e26727d68def55dbf01272159b6fe3d23b
Sebastian Osorio <83346917+SebasOso@users.noreply.github.com>
2023-08-20 17:37:42 -0500

    Effects

--- Assets/Scripts/HealthLogic/AxeDamage.cs
+++ Assets/Scripts/HealthLogic/AxeDamage.cs
@@ -7,6 +7,8 @@ public class AxeDamage : MonoBehaviour
     private List<Collider> alreadyColliderWith = new List<Collider>();
     [SerializeField] private Collider myCollider;
     [SerializeField] GameObject slash01;
+    [SerializeField] GameObject slash02;
+    [SerializeField] GameObject slash03;
     private float axeDamage;
     private void OnEnable() 
     {
@@ -15,10 +17,32 @@ public class AxeDamage : MonoBehaviour
     private void OnTriggerEnter(Collider other) 
     {
         if(other == myCollider){return;}
-        if(alreadyColliderWith.Contains(other)){return;}
+        if(alreadyColliderWith.Contains(other))
+        {   
+            slash01.SetActive(false);
+            return;
+        }
         alreadyColliderWith.Add(other);
         if(other.TryGetComponent<Health>(out Health health))
         {
+            switch (axeDamage)
+            {
+                case 16.5f: 
+                slash03.SetActive(true);
+                slash01.SetActive(false);
+                slash02.SetActive(false);
+                break;
+                case 16.9f: 
+                slash01.SetActive(true);
+                slash02.SetActive(false);
+                slash03.SetActive(false);
+                break;
+                case 21.3f: 
+                slash02.SetActive(true);
+                slash03.SetActive(false);
+                slash01.SetActive(false);
+                break;
+            }
             health.DealDamage(axeDamage);
         }
     }
