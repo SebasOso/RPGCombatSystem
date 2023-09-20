6bc1fc023c44d5bb3c2ad67cd5881b4282e87135
Sebastian Osorio <83346917+SebasOso@users.noreply.github.com>
2023-09-20 00:25:04 -0500

    Update EnemyStateMachine.cs

--- Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -9,6 +9,8 @@ using UnityEngine.AI;
 public class EnemyStateMachine : StateMachine
 {
     [field: SerializeField] 
+    public FieldOfView FieldOfView {get; private set;}
+    [field: SerializeField] 
     public EnemyLife HealthBar {get; private set;}
     [field: SerializeField] 
     public PatrolPath PatrolPath {get; private set;}
